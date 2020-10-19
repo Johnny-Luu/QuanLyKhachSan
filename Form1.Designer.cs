68f6c9a142a7b689aca5810a138012279672495b
annhienktuit <19520207@gm.uit.edu.vn>
2020-10-19 23:24:54 +0700

    them database + login

---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace QuanLyKhachSan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderEdges borderEdges1 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderEdges();
+            Bunifu.UI.WinForms.BunifuButton.BunifuButton.StateProperties stateProperties1 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton.StateProperties();
+            Bunifu.UI.WinForms.BunifuButton.BunifuButton.StateProperties stateProperties2 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton.StateProperties();
+            this.label1 = new System.Windows.Forms.Label();
+            this.username = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.password = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.bunifuButton1 = new Bunifu.UI.WinForms.BunifuButton.BunifuButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("San Francisco Display Med", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MediumPurple;
+            this.label1.Location = new System.Drawing.Point(146, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "HOTEL";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // username
+            // 
+            this.username.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
+            this.username.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
+            this.username.characterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.username.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.username.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.username.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.username.HintForeColor = System.Drawing.Color.Empty;
+            this.username.HintText = "";
+            this.username.isPassword = false;
+            this.username.LineFocusedColor = System.Drawing.Color.Blue;
+            this.username.LineIdleColor = System.Drawing.Color.Gray;
+            this.username.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.username.LineThickness = 3;
+            this.username.Location = new System.Drawing.Point(13, 184);
+            this.username.Margin = new System.Windows.Forms.Padding(4);
+            this.username.MaxLength = 32767;
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(349, 33);
+            this.username.TabIndex = 1;
+            this.username.Text = "Tài khoản";
+            this.username.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // password
+            // 
+            this.password.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.None;
+            this.password.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.None;
+            this.password.characterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.password.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.password.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.password.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.password.HintForeColor = System.Drawing.Color.Empty;
+            this.password.HintText = "";
+            this.password.isPassword = false;
+            this.password.LineFocusedColor = System.Drawing.Color.Blue;
+            this.password.LineIdleColor = System.Drawing.Color.Gray;
+            this.password.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.password.LineThickness = 3;
+            this.password.Location = new System.Drawing.Point(13, 260);
+            this.password.Margin = new System.Windows.Forms.Padding(4);
+            this.password.MaxLength = 32767;
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(349, 33);
+            this.password.TabIndex = 2;
+            this.password.Text = "Mật khẩu";
+            this.password.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.password.OnValueChanged += new System.EventHandler(this.bunifuMaterialTextbox2_OnValueChanged);
+            // 
+            // bunifuButton1
+            // 
+            this.bunifuButton1.AllowToggling = false;
+            this.bunifuButton1.AnimationSpeed = 200;
+            this.bunifuButton1.AutoGenerateColors = false;
+            this.bunifuButton1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuButton1.BackColor1 = System.Drawing.Color.DodgerBlue;
+            this.bunifuButton1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuButton1.BackgroundImage")));
+            this.bunifuButton1.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            this.bunifuButton1.ButtonText = "Đăng nhập";
+            this.bunifuButton1.ButtonTextMarginLeft = 0;
+            this.bunifuButton1.ColorContrastOnClick = 45;
+            this.bunifuButton1.ColorContrastOnHover = 45;
+            this.bunifuButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            borderEdges1.BottomLeft = true;
+            borderEdges1.BottomRight = true;
+            borderEdges1.TopLeft = true;
+            borderEdges1.TopRight = true;
+            this.bunifuButton1.CustomizableEdges = borderEdges1;
+            this.bunifuButton1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.bunifuButton1.DisabledBorderColor = System.Drawing.Color.Empty;
+            this.bunifuButton1.DisabledFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
+            this.bunifuButton1.DisabledForecolor = System.Drawing.Color.FromArgb(((int)(((byte)(168)))), ((int)(((byte)(160)))), ((int)(((byte)(168)))));
+            this.bunifuButton1.FocusState = Bunifu.UI.WinForms.BunifuButton.BunifuButton.ButtonStates.Pressed;
+            this.bunifuButton1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F);
+            this.bunifuButton1.ForeColor = System.Drawing.Color.White;
+            this.bunifuButton1.IconLeftCursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuButton1.IconMarginLeft = 11;
+            this.bunifuButton1.IconPadding = 10;
+            this.bunifuButton1.IconRightCursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuButton1.IdleBorderColor = System.Drawing.Color.DodgerBlue;
+            this.bunifuButton1.IdleBorderRadius = 3;
+            this.bunifuButton1.IdleBorderThickness = 1;
+            this.bunifuButton1.IdleFillColor = System.Drawing.Color.DodgerBlue;
+            this.bunifuButton1.IdleIconLeftImage = null;
+            this.bunifuButton1.IdleIconRightImage = null;
+            this.bunifuButton1.IndicateFocus = false;
+            this.bunifuButton1.Location = new System.Drawing.Point(116, 364);
+            this.bunifuButton1.Name = "bunifuButton1";
+            stateProperties1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(181)))), ((int)(((byte)(255)))));
+            stateProperties1.BorderRadius = 3;
+            stateProperties1.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            stateProperties1.BorderThickness = 1;
+            stateProperties1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(105)))), ((int)(((byte)(181)))), ((int)(((byte)(255)))));
+            stateProperties1.ForeColor = System.Drawing.Color.White;
+            stateProperties1.IconLeftImage = null;
+            stateProperties1.IconRightImage = null;
+            this.bunifuButton1.onHoverState = stateProperties1;
+            stateProperties2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(96)))), ((int)(((byte)(144)))));
+            stateProperties2.BorderRadius = 3;
+            stateProperties2.BorderStyle = Bunifu.UI.WinForms.BunifuButton.BunifuButton.BorderStyles.Solid;
+            stateProperties2.BorderThickness = 1;
+            stateProperties2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(96)))), ((int)(((byte)(144)))));
+            stateProperties2.ForeColor = System.Drawing.Color.White;
+            stateProperties2.IconLeftImage = null;
+            stateProperties2.IconRightImage = null;
+            this.bunifuButton1.OnPressedState = stateProperties2;
+            this.bunifuButton1.Size = new System.Drawing.Size(163, 45);
+            this.bunifuButton1.TabIndex = 3;
+            this.bunifuButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bunifuButton1.TextMarginLeft = 0;
+            this.bunifuButton1.UseDefaultRadiusAndThickness = true;
+            this.bunifuButton1.Click += new System.EventHandler(this.bunifuButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 553);
+            this.Controls.Add(this.bunifuButton1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox username;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox password;
+        private Bunifu.UI.WinForms.BunifuButton.BunifuButton bunifuButton1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace QuanLyKhachSan
+{
+    
+    public partial class Form1 : Form
+    {
+        SqlConnection database = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\TGDD\Documents\KhachSan.mdf;Integrated Security=True;Connect Timeout=30");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+      
+        
+
+        private void bunifuButton1_Click(object sender, EventArgs e)
+        {
+            database.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT COUNT(*) FROM NHANVIEN WHERE TenNhanVien = '"+username.Text+"' and MatKhau = '"+password.Text+"'",database);
+            DataTable dtb = new DataTable();
+            sda.Fill(dtb);
+            if (dtb.Rows[0][0].ToString() == "1")
+            {
+                Menu menuform = new Menu();
+                menuform.Show();
+                this.Hide();
+            }
+           
+            else
+            {
+                MessageBox.Show("Thông tin đăng nhập sai");
+            }
+            database.Close();
+
+
+
+            
+        }
+
+        private void bunifuMaterialTextbox2_OnValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
